b6f130b97f7bb949b60cc5cccef34641c2d18e03
aakashsolanke99 <aakashsolanke99@gmail.com>
2024-02-08 00:59:03 +0530

    push for deployment

--- src/main/java/com/revature/RevSpeed/controllers/AdminController.java
+++ src/main/java/com/revature/RevSpeed/controllers/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.List;
 
+// hi
 //@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/admin")
